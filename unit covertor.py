866dc4736d7cde34191c0bb93e1575ead704e382
amityadav36836 <amityaduvanshi1858@gmail.com>
2025-11-29 16:28:27 +0530

    Add files via upload

---
+++ unit covertor.py
@@ -0,0 +1,19 @@
+#Conversion kg to pounds:
+
+kg = float(input("Enter weight in kilograms: "))
+pounds = kg * 2.20462
+print("Pounds:", pounds)
+Pounds: 123.45871999999999
+#Conversion inr to usd:
+
+inr = float(input("Enter amount in rupees: "))
+usd = inr / 83
+print("Dollars:", usd)
+Dollars: 6.819277108433735
+#Conversion cm to inch:
+
+cm = float(input("Enter height in cm: "))
+inch = cm / 2.54
+print("Inches:", inch)
+Inches: 70.86614173228347
+#Conversion minutes to seconds:
